0ea7786fe36a37579ea50f140d356c1006237a75
Milad <miladxgh@outlook.com>
2025-10-06 11:41:37 +0330

    style: add framer motion animation to hero social links

--- components/ui/HeroSocial.tsx
+++ components/ui/HeroSocial.tsx
@@ -1,51 +1,57 @@
+import * as motion from "motion/react-client";
+// icons
 import { FaGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaCodepen } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
+const socialLinks = [
+  {
+    id: 0,
+    icon: <FaGithub />,
+    href: "https://github.com/micodex",
+    aria_label: "GitHub profile",
+  },
+  {
+    id: 1,
+    icon: <FaLinkedin />,
+    href: "https://www.linkedin.com/in/milad-gharibi-9ba94835a/",
+    aria_label: "linkedin profile",
+  },
+  {
+    id: 2,
+    icon: <FaCodepen />,
+    href: "https://codepen.io/mi_codex",
+    aria_label: "codepen profile",
+  },
+  {
+    id: 3,
+    icon: <FaInstagram />,
+    href: "https://github.com/micodex",
+    aria_label: "instagram profile",
+  },
+];
+
 const HeroSocial = () => {
   return (
-    <ul className="inline-flex space-x-4 p-1 rounded-full bg-gradient-to-r from-sky-200 to-indigo-200  dark:from-gray-900 dark:to-gray-800 ring-4 ring-indigo-200 dark:ring-gray-800">
-      <li className="flex text-3xl justify-center items-center w-14 h-14 rounded-full transition duration-100 dark:hover:bg-gray-700 cursor-pointer hover:text-sky-400 dark:text-gray-300">
-        <a
-          href="https://github.com/micodex"
-          target="_blank"
-          rel="noopener noreferrer" // for security and privacy
-          aria-label="GitHub profile"
+    <ul className="inline-flex space-x-4 p-1 rounded-full bg-gradient-to-r text-sky-900 dark:from-gray-900 dark:to-gray-800 ring-4 ring-sky-200 dark:ring-gray-800">
+      {socialLinks.map((link) => (
+        <li
+          key={link.id}
+          className="flex text-3xl justify-center transition items-center w-14 h-14 rounded-full dark:hover:bg-gray-700 hover:bg-sky-100 cursor-pointer hover:text-sky-600 dark:text-gray-300"
         >
-          <FaGithub />
-        </a>
-      </li>
-      <li className="flex text-3xl justify-center items-center w-14 h-14 rounded-full transition duration-150 dark:hover:bg-gray-700 cursor-pointer hover:text-sky-400 dark:text-gray-300">
-        <a
-          href="https://www.linkedin.com/in/milad-gharibi-9ba94835a/"
-          target="_blank"
-          rel="noopener noreferrer" // for security and privacy
-          aria-label="linkedin profile"
-        >
-          <FaLinkedin />
-        </a>
-      </li>
-      <li className="flex text-3xl justify-center items-center w-14 h-14 rounded-full transition duration-150 dark:hover:bg-gray-700 cursor-pointer hover:text-sky-400 dark:text-gray-300">
-        <a
-          href="https://codepen.io/mi_codex"
-          target="_blank"
-          rel="noopener noreferrer" // for security and privacy
-          aria-label="codepen profile"
-        >
-          <FaCodepen />
-        </a>
-      </li>
-      <li className="flex text-3xl justify-center items-center w-14 h-14 rounded-full transition duration-150 dark:hover:bg-gray-700 cursor-pointer hover:text-sky-400 dark:text-gray-300">
-        <a
-          href="https://www.instagram.com/web.script/"
-          target="_blank"
-          rel="noopener noreferrer" // for security and privacy
-          aria-label="instagram profile"
-        >
-          <FaInstagram />
-        </a>
-      </li>
+          <motion.a
+            whileHover={{ scale: 1.5 }}
+            whileTap={{ scale: 0.95 }}
+            href={link.href}
+            target="_blank" // open in new tab
+            rel="noopener noreferrer" // for security and privacy
+            aria-label={link.aria_label}
+          >
+            {link.icon}
+          </motion.a>
+        </li>
+      ))}
     </ul>
   );
 };
