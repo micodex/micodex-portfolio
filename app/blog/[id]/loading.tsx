4df66ba80d48f6ff5274fef7aeda42990f0ef470
Milad <miladxgh@outlook.com>
2025-12-04 14:06:05 +0330

    fix loading style for dark mode

--- app/blog/[id]/loading.tsx
+++ app/blog/[id]/loading.tsx
@@ -4,11 +4,11 @@ export default function Loading() {
       <div className="bg-gray-100 dark:bg-gray-900 px-0 pt-30 pb-10">
         <div className="max-w-[768px] mx-auto px-4">
           <div className="">
-            <div className="mb-8 w-32 h-8 rounded-full bg-gray-200 dark:bg-gray-950 "></div>
+            <div className="mb-8 w-32 h-8 rounded-full bg-gray-200 dark:bg-gray-800 animate-pulse"></div>
           </div>
-          <h1 className="mb-2 w-4/5 h-8 bg-gray-200"></h1>
-          <h1 className="mb-10 w-3/5 h-8 bg-gray-200"></h1>
-          <div className="px-0 mb-6 bg-gray-200 relative w-full aspect-16/9 border rounded-lg animate-pulse"></div>
+          <h1 className="mb-2 w-4/5 h-8 bg-gray-200 dark:bg-gray-800 animate-pulse"></h1>
+          <h1 className="mb-10 w-3/5 h-8 bg-gray-200 dark:bg-gray-800 animate-pulse"></h1>
+          <div className="px-0 mb-6 bg-gray-200 dark:bg-gray-800 relative w-full aspect-16/9 border rounded-lg animate-pulse"></div>
         </div>
       </div>
 
